22bd86c8f6eb7a6afd0e5619784b71fb0efab30a
Jake <jakedoyle8@gmail.com>
2025-11-09 18:39:14 +1100

    adding a 50 limit for inviting users every hour

--- src/organisation/invite/invite.controller.ts
+++ src/organisation/invite/invite.controller.ts
@@ -1,17 +1,20 @@
 import { Controller, Post, Delete, Body, UseGuards, Param } from '@nestjs/common';
+import { Throttle, minutes } from '@nestjs/throttler';
 import { InviteService } from './invite.service.js';
 import { InviteUserDto } from './dto/inviteUser.dto.js';
 import { RoleGuard } from '../../common/guards/role.guard.js';
 import { OrgMemberGuard } from '../../common/guards/orgMember.guard.js';
 import { RequiredRole } from '../../common/decorators/requiredRole.decorator.js';
 import { CurrentOrg } from '../decorators/currentOrg.decorator.js';
+import { UserThrottlerGuard } from 'src/common/guards/userThrottler.guard.js';
 
 @Controller('organisations/invite')
 export class InviteController {
   constructor(private readonly inviteService: InviteService) {}
 
   @Post()
-  @UseGuards(OrgMemberGuard, RoleGuard)
+  @UseGuards(OrgMemberGuard, RoleGuard, UserThrottlerGuard)
+  @Throttle({ default: { ttl: minutes(60), limit: 50 } })
   @RequiredRole('admin')
   async invite(@CurrentOrg() orgId: string, @Body() body: InviteUserDto) {
     return this.inviteService.invite(body.email, body.role, orgId);
