c23779f5b15b0e470f9071380bbb07c70d0d4d0f
Jake <jakedoyle8@gmail.com>
2026-03-14 16:02:48 +1100

    correctly typing types.ts

--- src/domain/organisation/get-current-org/getCurrentOrg.types.ts
+++ src/domain/organisation/get-current-org/getCurrentOrg.types.ts
@@ -1,8 +1,31 @@
 export type GetCurrentOrgRequest = Record<string, never>;
 
+export type CurrentOrgTeamMember = {
+  _id: string;
+  orgId: string;
+  email: string;
+  userId: string | null;
+  role: 'owner' | 'admin' | 'member' | 'invitee';
+  name: string | null;
+  image: string | null;
+  deletedAt: Date | null;
+  createdAt: Date;
+  updatedAt: Date;
+};
+
+export type CurrentOrgOrganisation = {
+  _id: string;
+  owner: string;
+  name: string;
+  timezone: string;
+  logo: string | null;
+  createdAt: Date;
+  updatedAt: Date;
+};
+
 export type CurrentOrgData = {
-  teamMember: Record<string, any>;
-  organisation: Record<string, any>;
+  teamMember: CurrentOrgTeamMember;
+  organisation: CurrentOrgOrganisation;
 };
 
 export type GetCurrentOrgResponse = {
